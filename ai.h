11e7414f1e2153f195c6fe2e2586119fed4fa4d7
kusano <kusano@users.noreply.github.com>
2016-03-09 04:11:02 +0900

    AIの基底クラスを作成

---
+++ ai.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "state.h"
+#include <string>
+
+struct Move
+{
+    int skill;  //  -1: none
+    int x, y;
+    int id;
+    char move[2][4];
+};
+
+class AI
+{
+public:
+    virtual std::string getName() = 0;
+    virtual Move think(State state[2], int turn, int time) = 0;
+};
